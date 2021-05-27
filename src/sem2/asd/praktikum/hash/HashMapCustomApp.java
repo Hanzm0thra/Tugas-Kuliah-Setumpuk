f04a2356cb9f4e803eb30efec248ad486a4a9f8e
Kylix <58837451+KylixEza@users.noreply.github.com>
2021-05-27 18:27:52 +0700

    update HashMapCustomApp.java

--- src/sem2/asd/praktikum/hash/HashMapCustomApp.java
+++ src/sem2/asd/praktikum/hash/HashMapCustomApp.java
@@ -1,9 +1,19 @@
 package sem2.asd.praktikum.hash;
 
+/**
+ Perbedaan:
+ * capacity
+ * hash function
+ nb: semua penjelasan akan berupa documentation comment sehingga mudah dibedakan
+ */
+
 class HashMapCustom<K, V> {
 
     private Entry<K,V>[] table;   //Array of Entry.
-    private int capacity= 4;  //Initial capacity of HashMap
+    /**
+     Untuk variabel capacity disini yang merupakan representasi dari ukuran array table nya, saya tingkatkan menjadi 19
+     **/
+    private int capacity= 19;  //Initial capacity of HashMap
 
     static class Entry<K, V> {
         K key;
@@ -23,36 +33,36 @@ class HashMapCustom<K, V> {
 
 
     public void put(K newKey, V data){
-        if(newKey==null)
+        if(newKey == null)
             return;    //does not allow to store null.
 
         //calculate hash of key.
-        int hash=hash(newKey);
+        int hash = hash(newKey);
 
         //create new entry.
         Entry<K,V> newEntry = new Entry<K,V>(newKey, data, null);
 
         //if table location does not contain any entry, store entry there.
-        if(table[hash] == null){
+        if(table[hash] == null) {
             table[hash] = newEntry;
-        }else{
+        } else {
             Entry<K,V> previous = null;
             Entry<K,V> current = table[hash];
 
             while(current != null){ //we have reached last entry of bucket.
                 if(current.key.equals(newKey)){
-                    if(previous==null){  //node has to be insert on first of bucket.
-                        newEntry.next=current.next;
-                        table[hash]=newEntry;
+                    if(previous == null){  //node has to be insert on first of bucket.
+                        newEntry.next = current.next;
+                        table[hash] = newEntry;
                         return;
                     }
                     else{
-                        newEntry.next=current.next;
-                        previous.next=newEntry;
+                        newEntry.next = current.next;
+                        previous.next = newEntry;
                         return;
                     }
                 }
-                previous=current;
+                previous = current;
                 current = current.next;
             }
             previous.next = newEntry;
@@ -60,68 +70,86 @@ class HashMapCustom<K, V> {
     }
 
 
-    public V get(K key){
+    public V get(K key) {
         int hash = hash(key);
-        if(table[hash] == null){
-            return null;
-        }else{
-            Entry<K,V> temp = table[hash];
-            while(temp!= null){
-                if(temp.key.equals(key))
+        //returns null if key is not found.
+        if (table[hash] != null) {
+            Entry<K, V> temp = table[hash];
+            while (temp != null) {
+                if (temp.key.equals(key))
                     return temp.value;
                 temp = temp.next; //return value corresponding to key.
             }
-            return null;   //returns null if key is not found.
         }
+        return null;
     }
 
-    public boolean remove(K deleteKey){
+    public boolean remove(K deleteKey) {
 
-        int hash=hash(deleteKey);
+        int hash = hash(deleteKey);
 
-        if(table[hash] == null){
-            return false;
-        }else{
-            Entry<K,V> previous = null;
-            Entry<K,V> current = table[hash];
+        if (table[hash] != null) {
+            Entry<K, V> previous = null;
+            Entry<K, V> current = table[hash];
 
-            while(current != null){ //we have reached last entry node of bucket.
-                if(current.key.equals(deleteKey)){
-                    if(previous==null){  //delete first entry node.
-                        table[hash]=table[hash].next;
+            while (current != null) { //we have reached last entry node of bucket.
+                if (current.key.equals(deleteKey)) {
+                    if (previous == null) {  //delete first entry node.
+                        table[hash] = table[hash].next;
                         return true;
-                    }
-                    else{
-                        previous.next=current.next;
+                    } else {
+                        previous.next = current.next;
                         return true;
                     }
                 }
-                previous=current;
+                previous = current;
                 current = current.next;
             }
-            return false;
         }
+        return false;
 
     }
 
 
-    public void display(){
+    public void display() {
 
-        for(int i=0;i<capacity;i++){
-            if(table[i]!=null){
-                Entry<K, V> entry=table[i];
-                while(entry!=null){
+        for (int i = 0; i < capacity; i++) {
+            if (table[i] != null) {
+                Entry<K, V> entry = table[i];
+                while(entry!=null) {
                     System.out.print("{"+entry.key+"="+entry.value+"}" +" ");
-                    entry=entry.next;
+                    entry = entry.next;
                 }
             }
         }
 
     }
 
+    /**
+     Untuk fungsi hash disini saya pisahkan menjadi 3 kemungkinan:
+     1. ketika key merupakan instance dari String:
+        ketika key berupa String maka akan dilakukan penambahan pada setiap char nya dimana setiap charnya dipresentasikan
+        dalam decimal number in ASCII TABLE
 
-    private int hash(K key){
-        return Math.abs(key.hashCode()) % capacity;
+     2. ketika key merupakan instance dari kelas Number beserta turunannya:
+        ketika key berupa turunan dari kelas Number makan memiliki fungsi -> Math.floor(capacity * Math.log(key.hashCode()))
+        yang dikonversi menjadi integer
+
+     3. ketika key bukan keduanya:
+        ketika key bukan berupa String maupun Number maka akan memiliki fungsi -> key.hashCode() + Math.log(capacity)
+        yang dikonversi menjadi integer
+     **/
+    public int hash(K key) {
+        int hashVal = 0;
+        if (key instanceof String) {
+            for (int i = 0; i < ((String) key).length(); i++)
+                hashVal = hashVal + ((String) key).charAt(i);
+        } else if (key instanceof Number) {
+            hashVal = (int) Math.floor(capacity * Math.log(key.hashCode()));
+        } else {
+            hashVal = (int) (key.hashCode() + Math.log(capacity));
+        }
+        return hashVal % capacity;
     }
 
 }
@@ -151,6 +179,6 @@ public class HashMapCustomApp {
 
         System.out.print("Displaying : ");
         hashMapCustom.display();
-
+        System.out.println();
     }
 }
